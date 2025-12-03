6024fd04d3dadff5530cd1aa43eb974bd701bf3a
Dimkom2 <dimkom82@gmail.com>
2025-12-03 18:23:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const CONFIG = {
         { login: "247", password: "Utka2022@", name: "Агент 247", chatId: "247" },
         { login: "001", password: "Pomidor:2022@", name: "Организатор", chatId: "001" },
         { login: "749", password: "Dinozavr456@", name: "Агент 749", chatId: "749" },
+        { login: "456", password: "Utka2022@", name: "Агент 456", chatId: "456" },
         { login: "947", password: "SigmaUbiyca654@", name: "Агент 947", chatId: "947" }
     ]
 }; 
@@ -740,5 +741,6 @@ document.addEventListener('DOMContentLoaded', function() {
     console.log('DOM загружен, запуск приложения...');
     window.initApp = initApp;
 });
+
 
 
